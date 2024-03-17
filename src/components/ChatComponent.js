75264411fed3f4c7145515450a9718ecc76dd2ae
KAZY9 <kazu.m.sora9@gmail.com>
2024-03-17 15:33:27 +0900

    change style

--- src/components/ChatComponent.js
+++ src/components/ChatComponent.js
@@ -1,11 +1,37 @@
 import React, { useState } from 'react';
 import { chat } from '../api/axios';
+import styled from 'styled-components';
+
+const Button = styled.button`
+        background-color: lightgray;
+        width: 80px;
+        height: 30px;
+        border: 1px solid gray;
+        border-radius: 50px;
+    `;
+
+const TextArea = styled.textarea`
+    width: 500px;
+    height: 200px;
+    border-radius: 5px;
+`;
+
+const ButtonArea = styled.div`
+    margin: 5px auto;
+`;
+
+const Element = styled.div`
+    margin: 50px auto;
+    text-align: center;
+    padding: 20px 0;
+`;
 
 const ChatComponent = () => {
-    const [ message, setMessage ] = useState( '' );
-    const [ answer, setAnswer ] = useState( '' );
-    const [ isLoading, setIsLoading ] = useState(false); 
-    
+    const [ message, setMessage ] = useState('');
+    const [ answer, setAnswer ] = useState('');
+    const [ isLoading, setIsLoading ] = useState(false);
+    const [ warning, setWarning ] = useState('');
+
     // メッセージを更新
     const handleMessageChange = (e)  => {
         setMessage(e.target.value);
@@ -14,7 +40,12 @@ const ChatComponent = () => {
     // 「質問」ボタンを押したときの処理
     const handleSubmit = async (e) => {
         e.preventDefault();
+        if(message === '') {
+            setWarning('質問を入力してください。');
+            return;
+        }
         setIsLoading(true);
+        setWarning('');
     
         // chat.js にメッセージを渡して API から回答を取得
         const responseText = await chat(message);
@@ -26,30 +57,30 @@ const ChatComponent = () => {
     }
 
     return (
-        <div>
+        <Element>
             <form onSubmit={handleSubmit}>
                 <label>
-                <textarea
-                    rows='5'
-                    cols='50'
+                <TextArea
                     value={message}
                     onChange={handleMessageChange}
                 />
                 </label>
-                <div>
-                    <button 
+                <ButtonArea>
+                    <Button
                         type="submit"
                         disabled={isLoading}
-                    >質問する</button>
-                </div>
+                    >質問する
+                    </Button>
+                </ButtonArea>
             </form>
+            {warning}
             {answer && (
                 <div>
                     <h2>回答:</h2>
                     <p>{answer}</p>
                 </div>
             )}
-        </div>
+        </Element>
     );
 }
  
